0d79b7b8307adf36fc35c0659e6c331f60035ea4
Kevin Thompson <kevinlthompson98@gmail.com>
2021-08-19 18:15:59 +0100

    add 'Friday' to dayOfWeek

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 // The final activity will comprise 4 strings randomly chosen from the below object
 
 const randomiser = {
-    dayOfWeek: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Saturday'],
+    dayOfWeek: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
     activity: ['go for a hike', 'take a weekend trip to a new city', 'plan a trip abroad', 'see a new movie at the cinema', 
                 'go on a camping trip', 'attend a music festival'],
     soloOrNot: ['by yourself', 'with your friends', 'with your family', 'with someone you recently met'],
