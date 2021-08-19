8f5ae8f8cc6bc40e2757ade66e9e6f08e93f2e6b
Kevin Thompson <kevinlthompson98@gmail.com>
2021-08-19 13:11:55 +0100

    create and display formatted message

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // This project is a Random Activity Generator 
 
-// The final activity will comprise 3 strings randomly chosen from the below object
+// The final activity will comprise 4 strings randomly chosen from the below object
 
 const randomiser = {
     dayOfWeek: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Saturday'],
@@ -9,3 +9,35 @@ const randomiser = {
     soloOrNot: ['by yourself', 'with your friends', 'with your family', 'with someone you recently met'],
     memories: ['take lots of pictures', 'write your experience in a journal', 'stream it live on Instagram', 'live in the moment']
 }
+
+const randIndex = (num) => {
+    return Math.floor(Math.random() * num)
+}
+
+// Store the activity in an array 
+let resolution = [];
+
+// Iterate over the object
+for (property in randomiser) {
+    let propertyIndex = randIndex(randomiser[property].length);
+
+    switch (property) {
+        case 'dayOfWeek':
+            resolution.push(`Your next resolution is due this coming ${randomiser[property][propertyIndex]}.`);
+            break
+        case 'activity':
+            resolution.push(`You've wanted to ${randomiser[property][propertyIndex]} for some time now. Let's get to it!`);
+            break
+        case 'soloOrNot':
+            resolution.push(`This time, you should do this ${randomiser[property][propertyIndex]}.`);
+            break    
+        case 'memories':
+            resolution.push(`Memories are very important, be sure to ${randomiser[property][propertyIndex]}.`);
+            break   
+    }
+}
+
+// Format the output of resolution
+const formatResolution = resolution => {console.log(resolution.join('\n'))};
+
+formatResolution(resolution);
